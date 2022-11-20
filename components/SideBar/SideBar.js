0e36767e9ae71b4d94dc7e74a5e8a56771645fbe
tawhidib <tawhidib@gmail.com>
2022-11-20 12:05:57 +0600

    sidebar done

--- components/SideBar/SideBar.js
+++ components/SideBar/SideBar.js
@@ -6,7 +6,9 @@ function SideBar() {
   return (
     <div className="px-6">
       <div className="">
-        <h4 className="text-lg font-semibold">About Me</h4>
+        <h4 className="text-lg font-semibold leading-10 border-y-2 text-center">
+          About Me
+        </h4>
         <img src={profile.src} className="w-60 h-60" />
         <p className="my-4 text-justify">
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -15,19 +17,38 @@ function SideBar() {
       </div>
 
       <div className="my-6">
-        <h4 className="text-lg font-semibold">Categories</h4>
-        <ul className="ml-10 list-disc">
-          <li className="text-sky-500 cursor-pointer">Life</li>
-          <li className="text-sky-500 cursor-pointer">Social</li>
-          <li className="text-sky-500 cursor-pointer">Sports</li>
-          <li className="text-sky-500 cursor-pointer">Technology</li>
-          <li className="text-sky-500 cursor-pointer">Health</li>
-          <li className="text-sky-500 cursor-pointer">Style</li>
+        <h4 className="text-lg font-semibold leading-10 border-y-2 text-center">
+          Categories
+        </h4>
+        <ul className="ml-12 mt-6 ">
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Life
+          </li>
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Social
+          </li>
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Sports
+          </li>
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Technology
+          </li>
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Health
+          </li>
+          <li className="text-sky-500 cursor-pointer w-[50%] inline-block mt-4">
+            Style
+          </li>
         </ul>
       </div>
 
       <div className="my-6">
-        <SidebarSocialIcons />
+        <h4 className="text-lg font-semibold leading-10 border-y-2 text-center">
+          Social Links
+        </h4>
+        <div className="py-6">
+          <SidebarSocialIcons />
+        </div>
       </div>
     </div>
   );
